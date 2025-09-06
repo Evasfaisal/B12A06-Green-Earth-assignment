0c3ed625068e7c443a87cf8b98a98c098fff2144
faisale <fa6769@gmaile.com>
2025-09-07 01:44:09 +0600

    running

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-// Dark green: #013223(nav / footer, headings, buttons)
+const categorieContainer = document.getElementById("CategorieContainer")
 
-// Medium green: #006747(gradients, hover)
 
-// Light green: #E6F4E6(sections background)
 
-// Yellow button: #FFD54F
+const loadcatagory = () => {
+    fetch('https://openapi.programming-hero.com/api/categories')
+        .then(res => res.json())
+        .then(data => {
 
-// Light mint: #DFF0DF
+            const catagories = data.categories
 
+            showcatagories(catagories)
+
+        })
+        .catch(err => {
+
+        })
+}
+const showcatagories = (catagories) => {
+
+
+    catagories.forEach(cat => {
+        console.log(cat.category_name)
+        categorieContainer.innerHTML += `
+
+</div>
+
+ 
+    <li id="${cat.id}" class="w-[250px] text-2xl hover:bg-green-900 p-3 hover:text-white rounded-lg mt-2" >${cat.category_name}</li>
+`
+    })
+}
+
+
+categorieContainer.addEventListener('click', (e)=> {
+   const allLi=document.querySelectorAll('li')
+   allLi.forEach(li =>{
+       li.classList.remove("bg-green-900", "text-white")
+   } )
+    if (e.target.localName
+=== "li" ){
+     
+        e.target.classList.add("bg-green-900", "text-white" )
+    }
+})
+
+
+
+
+
+
+
+
+loadcatagory()
 
