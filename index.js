c2da9129b125afa429b50b5ac4543b4ebd81fb85
faisale <fa6769@gmaile.com>
2025-09-06 22:10:30 +0600

    start

---
+++ index.js
@@ -0,0 +1,11 @@
+// Dark green: #013223(nav / footer, headings, buttons)
+
+// Medium green: #006747(gradients, hover)
+
+// Light green: #E6F4E6(sections background)
+
+// Yellow button: #FFD54F
+
+// Light mint: #DFF0DF
+
+
